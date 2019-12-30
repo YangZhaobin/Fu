98156406211d50674dcd73ec22b4d5db6af17cb6
yangzhaobin <yangzb0922@163.com>
2019-12-30 16:38:57 +0800

    1

--- src/main/java/com/yzb/test/collection/Test.java
+++ src/main/java/com/yzb/test/collection/Test.java
@@ -1,11 +1,22 @@
 package com.yzb.test.collection;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 public class Test {
     public static void main(String[] args) {
 
-        int i = (int) Math.pow(2, 30);
-        System.out.println(i);
-        System.out.println(1 << 30);
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+
+        list.removeIf(i -> i == 2);
+
+        System.out.println(list);
+
+
 
     }
 }
---
+++ src/main/java/com/yzb/test/collection/YList.java
@@ -0,0 +1,10 @@
+package com.yzb.test.collection;
+
+import java.util.ListIterator;
+import java.util.Objects;
+import java.util.function.UnaryOperator;
+
+public interface YList<E> extends YCollection<E> {
+
+
+}
